5face2d21929b65c1d45279804067d2528e9518a
wtygret <93733328+wtygret@users.noreply.github.com>
2021-12-16 14:22:40 -0500

    Add files via upload

--- packages/client/next.config.js
+++ packages/client/next.config.js
@@ -2,7 +2,7 @@ const isProduction = process.env.NODE_ENV === 'production';
 
 module.exports = {
     env: {
-        contentApi: 'sodagroupltd.cdn.content.amplience.net'
+        contentApi: 'labs.cdn.content.amplience.net'
     },
     poweredByHeader: false,
     target: 'serverless'
