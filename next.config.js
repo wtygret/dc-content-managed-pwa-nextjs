a74aeaa13d4f9afbce1398b3ed81f71733e724f9
wtygret <93733328+wtygret@users.noreply.github.com>
2021-12-28 10:06:27 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ const isProduction = process.env.NODE_ENV === 'production';
 
 module.exports = {
     env: {
-        contentApi: 'jaketest.cdn.content.amplience.net'
+        contentApi: 'sodagroupltd.cdn.content.amplience.net'
     },
     poweredByHeader: false,
     target: 'serverless'
